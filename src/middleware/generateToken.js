fd9110fa90071cbc63846d1217cd9497027e2f73
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-05-10 23:36:28 +0600

    create generate token.js for jwt

---
+++ src/middleware/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (user) => {
+  const payload = {
+    id: user.id,
+    email: user.email,
+    role: user.role,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.EXPIRES_JWT,
+  });
+
+  return token;
+};
+
+export default generateToken;
